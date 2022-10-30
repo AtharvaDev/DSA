c88b4bd8dab6356c8e7684092dbcb6f8f5894d1f
Atharva Deshmukh <atharva1525@gmail.com>
2022-10-30 12:35:02 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (99.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -16,8 +16,8 @@ class Solution {
     
     public int solve(int n, int[] dp){
         if (dp[n]!=-1) return dp[n]; 
-        if(n==1) return 1;
-        if(n==0) return 0;
+        if(n<=1) return n;
+        // if(n==0) return 0;
         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
 }
