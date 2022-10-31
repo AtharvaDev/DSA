653d0427d000e72eaf53b4d15be9c0bc9d1322cb
Atharva Deshmukh <atharva1525@gmail.com>
2022-10-31 09:26:15 +0530

    Added solution - LeetHub

---
+++ Subset Sums - GFG/subset-sums.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        // code here
+        Collections.sort(arr);
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        ans = solve(arr, N, ans, 0, 0);
+        return ans;
+    }
+    
+    ArrayList<Integer> solve(ArrayList<Integer> arr, 
+                int n, ArrayList<Integer> ans, int sum, int index){
+        
+        // if(index==n){
+        //     return ans;
+        // }
+        ans.add(sum);
+        for(int i=index; i<n; i++){
+            solve(arr, n, ans, sum+arr.get(i), i+1);
+        }
+        // solve(arr, n, ans, sum, index+1);
+        return ans;
+    }
+}
